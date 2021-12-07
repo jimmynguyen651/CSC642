25d4d4f8bde1231838eb133ecbbb9b2af551ffa6
RJ Perez <rhoannajo@gmail.com>
2021-12-06 23:05:04 -0800

    merged with jimmy's

---
+++ Components/resultspage.js
@@ -0,0 +1,81 @@
+var flexbox = document.getElementById("flexbox");
+var modal = document.getElementById("myModal");
+var filtersicon = document.getElementById("filtersicon");
+
+
+
+var numberclick1 = document.getElementById("numberclick");
+var numberclick2 = document.getElementById("numberclick2");
+var numberclick3 = document.getElementById("numberclick3");
+var chevronleft = document.getElementById("chevronleft");
+var chevronright = document.getElementById("chevronright");
+// rest of the clicks aren't added for the demo until it's deemed necessary
+
+
+var confirmfilters = document.getElementById("confirmFilters");
+var exitfilters = document.getElementById("exitFilters");
+var exitModal = document.getElementById("exitModal");
+var overlay = document.getElementById("overlay");
+
+filtersicon.onclick = function() {
+    modal.style.display = "block";
+    overlay.style.display = "block";
+}
+
+flexbox.onclick = function() {
+    // PUT RESTAURANT PAGE HERE
+     window.location.href = "restaurant-page.html"
+}
+// can add more buttons below here if we decide to direct them somewhere else, 
+// otherwise hitting the flexbox redirects to where we want to go
+
+// unable to hit multiple pages but our implementation does not necessarily need to call for such
+
+confirmfilters.onclick = function() {
+
+    // this function goes to resultspage2, which will contain vietnamese as an additional filter
+    window.location.href = "restaurant-page.html"
+    
+}
+exitfilters.onclick = function() {
+    // go to results page here
+    modal.style.display = "none";
+    overlay.style.display = "none";
+    
+}
+
+exitModal.onclick = function() {
+  
+    modal.style.display = "none";
+    overlay.style.display = "none";
+    
+}
+numberclick1.onclick = function() {
+    // this function goes to resultspage2, which will contain vietnamese as an additional filter
+    window.location.href = "restaurant-page.html"
+    
+}
+
+numberclick2.onclick = function() {
+    // this function goes to resultspage2, which will contain vietnamese as an additional filter
+    window.location.href = "restaurant-page.html"
+    
+}
+
+numberclick3.onclick = function() {
+    // this function goes to resultspage2, which will contain vietnamese as an additional filter
+    window.location.href = "restaurant-page.html"
+    
+}
+
+chevronleft.onclick = function() {
+    // this function goes to resultspage2, which will contain vietnamese as an additional filter
+    window.location.href = "restaurant-page.html"
+    
+}
+
+chevronright.onclick = function() {
+    // this function goes to resultspage2, which will contain vietnamese as an additional filter
+    window.location.href = "restaurant-page.html"
+    
+}
