ecf0f5672bd5de709df8a1e0d96ec24360d4aae9
RJ Perez <rhoannajo@gmail.com>
2021-12-08 13:15:12 -0800

    added reviews with star rating

--- Components/functional.js
+++ Components/functional.js
@@ -14,10 +14,10 @@ function addReview() {
         <p class="d-inline-block font-weight-bold">You</p>
         <p class="d-inline-block text-small"> • ` + dt.toLocaleDateString('en-GB', numeric) + `</p>
         <div class="margin-up pl-4 pt-2">
-            <span class="fa fa-star white-star unchecked-grey"></span>
-            <span class="fa fa-star white-star unchecked-grey"></span>
-            <span class="fa fa-star white-star unchecked-grey"></span>
-            <span class="fa fa-star white-star unchecked-grey"></span>
+            <span class="fa fa-star white-star checked-yellow"></span>
+            <span class="fa fa-star white-star checked-yellow"></span>
+            <span class="fa fa-star white-star checked-yellow"></span>
+            <span class="fa fa-star white-star checked-yellow"></span>
             <span class="fa fa-star white-star unchecked-grey"></span><br/>
         </div>
         <p class="ml-4 mr-4 pt-2"> ` + review +`</p>
