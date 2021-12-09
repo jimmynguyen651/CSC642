b7924c08cde2031b2a796c510040a81a2295f1c5
jimmynguyen651 <56140887+jimmynguyen651@users.noreply.github.com>
2021-12-08 19:06:23 -0800

    Update resultspage.js

--- resultspage.js
+++ resultspage.js
@@ -11,6 +11,15 @@ var chevronleft = document.getElementById("chevronleft");
 var chevronright = document.getElementById("chevronright");
 // rest of the clicks aren't added for the demo until it's deemed necessary
 
+// NOTES FROM JIMMY IF YOU'RE CONFUSED:
+// - Results Page 2 is a hardcoded example of filters working
+// - Results Page 3 is an extra page we swap to when clicking on "2" or the right arrow
+
+// FOR HTML:
+// - CHANGE FUNNY SUNGLASSES CAT PIC TO RESTAURANT ON LINES 77 AND 157
+
+// MISC:
+// - MAKE SEARCH BAR KEEP "VEGETARIAN" AS SEARCH RESULTS FOR CONSISTENCY
 
 var confirmfilters = document.getElementById("confirmFilters");
 var exitfilters = document.getElementById("exitFilters");
@@ -24,21 +33,16 @@ filtersicon.onclick = function() {
 
 flexbox.onclick = function() {
     // PUT RESTAURANT PAGE HERE
-     window.location.href = "test.html"
+     window.location.href = "restaurantpage.html"
 }
-// can add more buttons below here if we decide to direct them somewhere else, 
-// otherwise hitting the flexbox redirects to where we want to go
-
-// unable to hit multiple pages but our implementation does not necessarily need to call for such
 
 confirmfilters.onclick = function() {
 
-    // this function goes to resultspage2, which will contain vietnamese as an additional filter
-    window.location.href = "test.html"
+    // this function goes to resultspage2, which will contain Japanese as an additional filter
+    window.location.href = "resultspage2.html"
     
 }
 exitfilters.onclick = function() {
-    // go to results page here
     modal.style.display = "none";
     overlay.style.display = "none";
     
@@ -51,31 +55,26 @@ exitModal.onclick = function() {
     
 }
 numberclick1.onclick = function() {
-    // this function goes to resultspage2, which will contain vietnamese as an additional filter
-    window.location.href = "test.html"
+    // goes to FIRST results page (default)
+    window.location.href = "resultspage2.html"
     
 }
 
 numberclick2.onclick = function() {
-    // this function goes to resultspage2, which will contain vietnamese as an additional filter
-    window.location.href = "test.html"
+    // goes to resultspage2 (second)
+    window.location.href = "resultspage3.html"
     
 }
 
-numberclick3.onclick = function() {
-    // this function goes to resultspage2, which will contain vietnamese as an additional filter
-    window.location.href = "test.html"
-    
-}
 
 chevronleft.onclick = function() {
-    // this function goes to resultspage2, which will contain vietnamese as an additional filter
-    window.location.href = "test.html"
+    // For the sake of the demo, this goes back to the first page with the filter activated 
+    window.location.href = "resultspage2.html"
     
 }
 
 chevronright.onclick = function() {
-    // this function goes to resultspage2, which will contain vietnamese as an additional filter
-    window.location.href = "test.html"
+    // goes to resultspage2 (second)
+    window.location.href = "resultspage3.html"
     
 }
